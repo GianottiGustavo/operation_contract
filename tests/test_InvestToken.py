6cce52d60175be326f073b9ff78fae5c82af5d53
GianottiGustavo <gianotti.gustavo@gmail.com>
2022-01-17 20:57:12 -0300

    add fixture

--- tests/test_InvestToken.py
+++ tests/test_InvestToken.py
@@ -2,6 +2,13 @@ from brownie import InvestToken, accounts
 import pytest
 
 
+@pytest.fixture
+def inv_tok() -> InvestToken:
+    account = accounts[0]
+    inv_tok = InvestToken.deploy("InvestToken", "IT", {"from": account})
+    return inv_tok
+
+
 def test_deploy_totalSupply():
     # Arrange
     account = accounts[0]
@@ -22,40 +29,33 @@ def test_owner():
     assert contract_owner == account
 
 
-def test_mint_balanceOf():
-    # Arrange
-    account = accounts[0]
-    inv_tok = InvestToken.deploy("InvestToken", "IT", {"from": account})
-    # Act
+def test_mint_balanceOf(inv_tok):
+    # Arrange/Act
     account_invst = accounts[1]
     amount = 10
-    inv_tok.mint(account_invst, amount, {"from": account})
+    inv_tok.mint(account_invst, amount, {"from": inv_tok.owner()})
     # Assert
     assert inv_tok.balanceOf(account_invst) == amount
 
 
-def test_mint_disburse_one_acc_withdraw():
+def test_mint_disburse_one_acc_withdraw(inv_tok):
     # Arrange
-    account = accounts[0]
-    inv_tok = InvestToken.deploy("InvestToken", "IT", {"from": account})
     acc_invst = accounts[1]
     acc_balance_pre_withdraw = acc_invst.balance()
-    inv_tok.mint(acc_invst, 10, {"from": account})
+    inv_tok.mint(acc_invst, 10, {"from": inv_tok.owner()})
     # Act
-    inv_tok.disburse({"from": account, "value": 10})
+    inv_tok.disburse({"from": inv_tok.owner(), "value": 10})
     inv_tok.withdraw({"from": acc_invst})
     acc_balance_with = acc_invst.balance()
     # Assert
     assert acc_balance_pre_withdraw + 10 == acc_balance_with
 
 
-def test_transfer():
+def test_transfer(inv_tok):
     # Arrange
-    account = accounts[0]
-    inv_tok = InvestToken.deploy("InvestToken", "IT", {"from": account})
     account_invst = accounts[1]
     amount_mint = 10
-    inv_tok.mint(account_invst, amount_mint, {"from": account})
+    inv_tok.mint(account_invst, amount_mint, {"from": inv_tok.owner()})
 
     # Act
     account_invst_new = accounts[2]
@@ -65,41 +65,37 @@ def test_transfer():
     assert inv_tok.balanceOf(account_invst_new) == amount_mint
 
 
-def test_transfer_from():
+def test_transfer_from(inv_tok):
     # Arrange
-    account = accounts[0]
-    inv_tok = InvestToken.deploy("InvestToken", "IT", {"from": account})
     account_invst = accounts[1]
     amount_mint = 10
-    inv_tok.mint(account_invst, amount_mint, {"from": account})
+    inv_tok.mint(account_invst, amount_mint, {"from": inv_tok.owner()})
 
     # Act
     account_invst_new = accounts[2]
 
-    tx = inv_tok.approve(account, amount_mint, {"from": account_invst})
+    tx = inv_tok.approve(inv_tok.owner(), amount_mint, {"from": account_invst})
     tx.wait(1)
     inv_tok.transferFrom(
-        account_invst, account_invst_new, amount_mint, {"from": account}
+        account_invst, account_invst_new, amount_mint, {"from": inv_tok.owner()}
     )
 
     # Assert
     assert inv_tok.balanceOf(account_invst_new) == amount_mint
 
 
-def test_disburse_transfer():
+def test_disburse_transfer(inv_tok):
     # Arrane
-    account = accounts[0]
-    inv_token = InvestToken.deploy("InvestToken", "IT", {"from": account})
     account_invst = accounts[1]
     account_inv_balance_pre = account_invst.balance()
     ammount_mint = 234
-    inv_token.mint(account_invst, ammount_mint, {"from": account})
+    inv_tok.mint(account_invst, ammount_mint, {"from": inv_tok.owner()})
     ammount_disburse = 9898398584975935
-    inv_token.disburse({"from": account, "value": ammount_disburse})
+    inv_tok.disburse({"from": inv_tok.owner(), "value": ammount_disburse})
 
     # Act
     account_invst_new = accounts[2]
-    inv_token.transfer(account_invst_new, 1, {"from": account_invst})
+    inv_tok.transfer(account_invst_new, 1, {"from": account_invst})
 
     # Assert
     assert (
@@ -108,24 +104,22 @@ def test_disburse_transfer():
     )
 
 
-def test_disburse_transferFrom():
+def test_disburse_transferFrom(inv_tok):
     # Arrane
-    account = accounts[0]
-    inv_token = InvestToken.deploy("InvestToken", "IT", {"from": account})
     account_invst = accounts[1]
     account_inv_balance_pre = account_invst.balance()
     ammount_mint = 43567
-    inv_token.mint(account_invst, ammount_mint, {"from": account})
+    inv_tok.mint(account_invst, ammount_mint, {"from": inv_tok.owner()})
     ammount_disburse = 987656789654
-    inv_token.disburse({"from": account, "value": ammount_disburse})
+    inv_tok.disburse({"from": inv_tok.owner(), "value": ammount_disburse})
 
     # Act
     account_invst_new = accounts[2]
 
-    tx = inv_token.approve(account, ammount_mint, {"from": account_invst})
+    tx = inv_tok.approve(inv_tok.owner(), ammount_mint, {"from": account_invst})
     tx.wait(1)
-    inv_token.transferFrom(
-        account_invst, account_invst_new, ammount_mint, {"from": account}
+    inv_tok.transferFrom(
+        account_invst, account_invst_new, ammount_mint, {"from": inv_tok.owner()}
     )
 
     # Assert
@@ -148,17 +142,15 @@ def test_disburse_transferFrom():
         (1, [927, 603, 25, 24, 86, 1, 2, 3, 1]),
     ],
 )
-def test_mint_disburse_nine_acc_withdraw(j, weights):
+def test_mint_disburse_nine_acc_withdraw(inv_tok, j, weights):
     # Arrange
-    account = accounts[0]
-    inv_tok = InvestToken.deploy("InvestToken", "IT", {"from": account})
     acc_invst = [accounts[i] for i in range(1, 10)]
     acc_balance_pre_withdraw = [acc_invst[i].balance() for i in range(9)]
     for i, k in zip(range(9), weights):
-        inv_tok.mint(acc_invst[i], k, {"from": account})
+        inv_tok.mint(acc_invst[i], k, {"from": inv_tok.owner()})
     # Act
     disburse_amm = j
-    inv_tok.disburse({"from": account, "value": disburse_amm})
+    inv_tok.disburse({"from": inv_tok.owner(), "value": disburse_amm})
     for i in range(9):
         inv_tok.withdraw({"from": acc_invst[i]})
     acc_balance_with = [acc_invst[i].balance() for i in range(9)]
@@ -186,17 +178,15 @@ def test_mint_disburse_nine_acc_withdraw(j, weights):
         (1, 45657567745, [927, 603, 25, 24, 86, 1, 2, 3, 1]),
     ],
 )
-def test_disburse_withdraw_withdraw_disburse_withdraw(disb_1, disb_2, weights):
+def test_disburse_withdraw_withdraw_disburse_withdraw(inv_tok, disb_1, disb_2, weights):
     # Arrange
-    account = accounts[0]
-    inv_tok = InvestToken.deploy("InvestToken", "IT", {"from": account})
     acc_invst = [accounts[i] for i in range(1, 10)]
     acc_balance_pre_withdraw = [acc_invst[i].balance() for i in range(9)]
     for i, j in zip(range(9), weights):
-        inv_tok.mint(acc_invst[i], j, {"from": account})
+        inv_tok.mint(acc_invst[i], j, {"from": inv_tok.owner()})
     # Act I
     disburse_amm = disb_1
-    inv_tok.disburse({"from": account, "value": disburse_amm})
+    inv_tok.disburse({"from": inv_tok.owner(), "value": disburse_amm})
     for i in range(4):
         inv_tok.withdraw({"from": acc_invst[i]})
     for i in range(4):
@@ -213,7 +203,7 @@ def test_disburse_withdraw_withdraw_disburse_withdraw(disb_1, disb_2, weights):
         )
     # Act II
     disburse_amm = disb_2
-    inv_tok.disburse({"from": account, "value": disburse_amm})
+    inv_tok.disburse({"from": inv_tok.owner(), "value": disburse_amm})
     for i in range(9):
         inv_tok.withdraw({"from": acc_invst[i]})
     acc_balance_with = [acc_invst[i].balance() for i in range(9)]
